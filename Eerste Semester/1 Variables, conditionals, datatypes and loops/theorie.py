3bc5e314e982ebddebdf861d99164726337f6246
thisaccountisformyschool <lars.vandezande@vtileuven.be>
2026-09-09 12:00:21 +0200

    I'm commiting it

---
+++ Eerste Semester/1 Variables, conditionals, datatypes and loops/theorie.py
@@ -0,0 +1,54 @@
+import math
+
+x = 5
+#x is a veriable, an integer
+
+z = "EVERYONE GET IN THE CAR WE'RE LEAVING THIS TOWN NOW " #WHAT THE, THIS ISN'T THE CAR!? IMPOSSIBLE! AAHHHHH NOOOOOO AAAAAHHHHH HERMIT PURPLE
+
+y = 3.14
+print(x)
+print(y)
+print(z) #z is a variable btw a string
+
+x = x+5
+print(x)
+#holy shittings man x is 10 now
+x = x * 2
+
+print(x)
+
+x = x / 2
+
+print(x)
+x=int(x)
+
+x = x^2
+
+print(x)
+
+z = z + "WHAT THE, THIS ISN'T THE CAR!? IMPOSSIBLE! AAHHHHH NOOOOOO AAAAAHHHHH HERMIT PURPLE"
+
+print(z)
+
+lijstje = [x,y,z,"oh shittings man"]
+
+lijstje.remove("oh shittings man")
+lijstje.remove(y)
+
+print(lijstje)
+
+print(lijstje.pop(0))
+lijstje.append("giraffe")
+
+print(lijstje)
+
+lijstje.insert(0,y)
+
+print(lijstje)
+
+listicus2icus = [1,4,862,3,2,33]
+print(listicus2icus)
+print(lijstje + listicus2icus)
+
+lijstje.append(listicus2icus)
+print(lijstje)
